f1447078cd1315427b8cd12939a751c9126bdf7c
Georgio Tumbado <97042086+JorgeRico08@users.noreply.github.com>
2023-10-11 12:12:43 -0600

    Modulo para login como usuario admistrador HU-03

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const isAdmin = require('../middlewares/isAdmin');
+const authenticateToken = require("../middlewares/authenticateToken");
+
+var AdminController = require('../controllers/adminController');
+const admin = new AdminController();
+
 /* GET home page. */
-router.get('/admin', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/admin/adminDashboard', authenticateToken, isAdmin, admin.getDashboard.bind(admin));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ var router = express.Router();
 const UserController = require('../controllers/userController');
 const userController = new UserController();
 const authenticateToken = require("../middlewares/authenticateToken");
-
+const isUser = require("../middlewares/isUser");
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/users/crearPublicacion', authenticateToken, userController.crearPublicacion.bind(userController));
-router.get('/users/verPublicacion', authenticateToken, userController.verPublicacion.bind(userController));
+router.post('/users/crearPublicacion', authenticateToken, isUser , userController.crearPublicacion.bind(userController));
+router.get('/users/verPublicacion', authenticateToken, isUser,userController.verPublicacion.bind(userController));
 
 
 module.exports = router;
